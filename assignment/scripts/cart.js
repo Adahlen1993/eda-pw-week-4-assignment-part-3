752d37d407d20574a976c9445bb7840c55a5b5a3
Alex <adahl1993@gmail.com>
2024-04-14 15:49:13 -0500

    finished problems up to stretch goal

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,41 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+
+function addItem(item) {
+basket.push(item);
+return true;
+}
+
+console.log('basket contains', basket);
+console.log('Adding item to basket', addItem('olives'));
+console.log('basket contains', basket);
+
+console.log('adding new item', addItem('cheese'));
+console.log('basket contains', basket);
+
+console.log('adding new item', addItem('peppers'));
+console.log('basket contains', basket);
+
+function listItems() {
+    for (i = 0; i < basket.length; i++) {
+        console.log(basket[i]);
+    }
+};
+
+console.log('adding new item', addItem('rice'));
+listItems();
+
+function empty() {
+    while (basket.length > 0) {
+        basket.pop();
+    }
+}
+
+console.log(basket);
+
+
 
 
 
